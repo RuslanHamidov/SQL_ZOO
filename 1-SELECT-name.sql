4b9dccfaf5998c14664681f82782b4dd3aa1c6f8
RuslanHamidov <rhamidov202@gmail.com>
2024-07-10 13:08:05 +0400

    add final 1-select

--- 1-SELECT-name.sql
+++ 1-SELECT-name.sql
@@ -12,3 +12,39 @@ SELECT name FROM world
 
 SELECT name FROM world
   WHERE name LIKE '%x%';
+
+-- The countries that end with land
+
+SELECT name FROM world
+  WHERE name LIKE '%land';
+
+-- The countries that start with C and end with ia
+
+SELECT name FROM world
+  WHERE name LIKE 'C%ia';
+
+-- The country that has oo in the name
+
+SELECT name FROM world
+  WHERE name LIKE '%oo%';
+
+-- The countries that have three or more a in the name
+
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%';
+
+-- The countries that have "t" as the second character
+
+SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name;
+
+-- The countries that have two "o" characters separated by two others
+
+SELECT name FROM world
+ WHERE name LIKE '%o__o%';
+
+-- The countries that have exactly four characters
+
+SELECT name FROM world
+ WHERE name LIKE '____';
